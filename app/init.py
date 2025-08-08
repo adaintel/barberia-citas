736880e7c4487f6fad1bedba490be9c8defca2ef
negro312 <vpnoriente@gmail.com>
2025-08-08 08:46:36 -0400

    Update init.py
    ssss

--- app/init.py
+++ app/init.py
@@ -1,26 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+    
     db.init_app(app)
     migrate.init_app(app, db)
-    login_manager.init_app(app)
-
+    
+    # Registrar blueprints
     from app.routes import main_routes, auth_routes, cita_routes, admin_routes
     app.register_blueprint(main_routes)
     app.register_blueprint(auth_routes, url_prefix='/auth')
     app.register_blueprint(cita_routes, url_prefix='/citas')
     app.register_blueprint(admin_routes, url_prefix='/admin')
-
+    
     return app
